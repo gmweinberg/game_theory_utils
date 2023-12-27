62b87cc70f4ba90666d8da367297557ee3d523e0
George Weinberg <gmweinberg@fastmail.fm>
2023-12-27 13:17:05 -0800

    add class

--- src/game_theory_utils/coalitions/shapley.py
+++ src/game_theory_utils/coalitions/shapley.py
@@ -4,6 +4,69 @@ from itertools import chain, combinations, permutations
 from collections import defaultdict
 from math import factorial
 
+class Shapley:
+    def __init__(self, vals=None):
+        self.players = set()
+        self.shapely_vals = None
+        if vals:
+            self.filled_vals = self.get_filled_vals()
+
+    def get_filled_vals(vals):
+        """Given a dictionary of subset values, 
+            fill in any missing values according to the get_shapely_value rules.
+            Fills in players as a side effect."""
+        vals = _frozen_vals(vals)
+        players = set()
+        for key in vals.keys():
+            for elm in key:
+                 players.add(elm)
+        fv = {}
+        for elm in powerset(players):
+            frozen = frozenset(elm)
+            if len(elm) == 0:
+                fv[frozen] = 0
+            elif frozen in vals:
+                fv[frozen] = vals[frozen]
+            elif len(frozen) == 1:
+                fv[frozen] = 0
+            else:
+                max_ = None
+                s = list(frozen)
+                for shorter in combinations(s, len(frozen) - 1):
+                    new = fv[frozenset(shorter)]
+                    if max_ is None:
+                        max_ = new
+                    elif new > max_:
+                        max_ = new
+                fv[frozen] = max_
+        self.players = players
+        return fv
+
+    def compute_shapley_vals(self):
+        shapley = defaultdict(float)
+        players = self.players
+
+        for combo in permutations(players):
+            for ii, elm in enumerate(combo):
+                player = combo[ii]
+                if ii == 0:
+                    shapley[player] += vals[frozenset([player])]
+                else:
+                    prev_players = combo[0:ii]
+                    cur_players = combo[0:ii + 1]
+                    shapley[player] += vals[frozenset(cur_players)] - vals[frozenset(prev_players)]
+
+        div = factorial(len(players))
+        for player in shapley.keys():
+            shapley[player] = shapley[player]/div
+
+        self.shapley_vals = shapley 
+
+
+    def get_shapley_vals(self):
+        return self.shapley_vals
+        
+
 
 def get_shapley_values(vals):
     """Given a dictionary of values of subsets of players, calculate the shapely value for
@@ -67,6 +130,31 @@ def _filled_vals(vals):
                     max_ = new
             fv[frozen] = max_
     return fv, players
+
+def voting_shapley(vals):
+    """Find the shapely power for the simplified case where all players have a voting power and majority wins.
+       Vals is a dict identifier: votes"""
+    # For this function to be useful the voters have to be grouped into types with the same voting power.
+    # we will identify the types just with ints
+    types = defaultdict(list) # key = type, val = original players of that type
+    powers = dict() # key = type, val = power of that type
+    ipower = dict() # inverse of power. key = power, val = type
+    newtype = 0
+    total = 0 # total voting power
+    for key in vals.keys():
+        power = val[key]
+        total += power
+        if power in ipower:
+            type_ = ipower[power]
+            types[type_].append(key)
+        else:
+            powers[newtype] = power
+            ipower[power] = newtype
+            types[newtype].append(key)
+            newtype += 1
+
+            
+    pass
 
 
 def powerset(iterable):
